c0cf462afb8be33ec72a06d35c03ca31e6add195
TerriAnn Walker <102176320+terriberri82@users.noreply.github.com>
2025-11-30 15:30:23 -0500

    add TodoList and TodoForm Components

---
+++ src/TodoForm.jsx
@@ -0,0 +1,12 @@
+function TodoForm(){
+
+    return (
+        <form >
+            <label htmlFor="todoTitle">Todo</label>
+            <input id="todoTitle"></input>
+            <button>Add Todo</button>
+        </form>
+    )
+}
+
+export default TodoForm

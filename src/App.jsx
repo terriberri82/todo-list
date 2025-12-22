6b246d7e8ac283a084ac00f8121179a72a83b518
TerriAnn Walker <102176320+terriberri82@users.noreply.github.com>
2025-12-22 03:37:08 -0500

    Revert "Revert "add TodoListItem and modified App and TodoList components""
    This reverts commit 915d064fed405d50f4565db45665e97050a39809.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
+import { useState } from 'react';
 import './App.css'
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx'
 function App() {
-
+const [newTodo, setNewTodo]= useState("Call Mom");
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
       
     </div>
